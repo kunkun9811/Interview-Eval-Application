16456dd46145565ad3f323981fde09522e00d77d
Tom Shih <tshih98@gmail.com>
2020-12-23 15:44:35 -0800

    add route to process post request

--- server/server.py
+++ server/server.py
@@ -1,6 +1,19 @@
-from flask import flask
+from flask import Flask, request
+import json
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World'
+
+@app.route('/datatext', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # data will be json of the form {1: "p1", 2: "p2"}
+        #convert into dictionary
+        data = request.json
+        article = "";
+        for key, value in data:
+            article += value + " "
+        print(article)
