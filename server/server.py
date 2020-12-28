fa0408e115d1fa2b37737335d6ce3fe04933545c
Tom Shih <tshih98@gmail.com>
2020-12-28 14:52:39 -0800

    added function skeleton for summarizer

--- server/server.py
+++ server/server.py
@@ -3,10 +3,17 @@ import json
 import pandas as pd
 app = Flask(__name__)
 
-def writeToFile(article, filename):
-    f = open(filename, "w");
+def writeToFile(article, fileName):
+    f = open(fileName, "w");
     f.write(article)
-    f.close
+    f.close()
+
+def generateSummary(fileName):
+    f = open(fileName, "r")
+    message = f.readline()
+    f.close()
+    print 
+    return message[:35]
 
 @app.route('/')
 def hello_world():
@@ -14,6 +21,7 @@ def hello_world():
 
 @app.route('/datatext', methods=['POST'])
 def upload_file():
+    fileName = "testarticle.txt"
     if request.method == 'POST':
         # data will be json of the form {1: "p1", 2: "p2"}
         #convert into dictionary
@@ -23,8 +31,9 @@ def upload_file():
         for key, value in text.items():
             article += value + " "
         
-        writeToFile(article, "testarticle.txt")
+        writeToFile(article, fileName)
 
         print("written to file")
-        res = make_response(jsonify({"message": "YAY"}), 200)
+        message = generateSummary(fileName)
+        res = make_response(jsonify({"message": message}), 200)
         return res
